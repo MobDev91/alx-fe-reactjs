5dc9e7c96ef8f7c25ff361ab7c839d33eb69eaa5
oussama.benyssef <oussama.benyssef@gmail.com>
2024-11-17 18:24:42 +0100

    Advanced features to Recipe sharing App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,15 +3,21 @@ import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/RecipeList'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeDetails from './RecipeDetails';
+import SearchBar from './SearchBar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
-      <Routes>
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
+      <div>
+        <SearchBar />
+        <RecipeList />
+        <Routes>
+          {/* Other routes */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,9 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
   addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map((recipe) =>
@@ -10,6 +13,11 @@ const useRecipeStore = create((set) => ({
   })),
   deleteRecipe: (recipeId) => set((state) => ({
     recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
   })),
 }));
 
